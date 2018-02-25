109a2645e5dc2f97f07b555bea3d57227cbd5e20
YomnaGad <yomnagad8@gmail.com>
2018-02-25 13:12:51 +0200

    added some movements

---
+++ src/com/chess/engine/pieces/Knight.java
@@ -0,0 +1,47 @@
+package com.chess.engine.pieces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
+
+public class Knight extends Piece{
+	
+	private final static int[] CANDIDATE_MOVE_COORDINATE ={-17, -15, -10, -6, 6, 10, 15, 17};
+	public Knight(final int piecePosition, final Alliance pieceAlliance) {
+		super(piecePosition, pieceAlliance);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Move> calculateLegalMoves(Board board) {
+		
+		int candidateDestinationCoordinate;
+		final List<Move> legalMoves = new ArrayList<>();
+		for(final int currentCandidate : CANDIDATE_MOVE_COORDINATE){
+			candidateDestinationCoordinate = this.piecePosition + currentCandidate;
+			if(true/*is valid tile coordinate*/){
+				final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+				if(!candidateDestinationTile.isTileOccupied()){
+					legalMoves.add(new Move());
+				}
+				else{
+					final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+					final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+					if(this.pieceAlliance != pieceAlliance){
+						legalMoves.add(new Move());
+					}
+				}
+			
+			}
+		}
+		// TODO Auto-generated method stub
+		return ImmutableList.copyOf(legalMoves);
+	}
+
+}
+ 
